05d1136358ffa1a7fc66e269cb421a2c7e5f17d1
E BOUTALEB <kagekyio@gmail.com>
2015-12-19 13:06:43 +0100

    Cleaning the code

--- XOR/XOR.cpp
+++ XOR/XOR.cpp
@@ -23,71 +23,98 @@ namespace {
             }
 
             bool runOnBasicBlock(BasicBlock &BB) override {
+
                 bool modified = false;
 
                 for (auto IIT = BB.begin(), IE = BB.end(); IIT != IE; ++IIT) {
                     Instruction &Inst = *IIT;
 
+                    if (isValidCandidateInstruction(Inst)) {
+                        if (isValidCandidateOperand(Inst.getOperand(0))) {
 
-                    DEBUG(dbgs() << Inst.getOperand(0)->getValueName() << "\n");
-                    DEBUG(dbgs() << Inst.getNumOperands() << "\n");
-                    //DEBUG(dbgs() << Inst.getOperand(1)->getName() << "\n");
-                    //DEBUG(dbgs() << Inst.getOperand(1)->getValueName() << "\n");
-                    DEBUG(dbgs() << "................" << "\n");
+                            Value *NewValue = replaceZero(Inst);
 
-                    IRBuilder<> Builder(&Inst);
+                            // Testing the new value for the null pointer triggers an error
+                            // "Terminator found in the middle of a basic block!" : why?
+                            ReplaceInstWithValue(BB.getInstList(), IIT, NewValue);
 
-                    StoreInst *SI = dyn_cast<StoreInst>(IIT);
+                            // Consecutive zero assignments causes some instructions to be skipped.
+                            // The instruction iterator is decremented to avoid this.
+                            //
+                            // Concerning the return instruction, without this,
+                            // the pass would just perform the transformation forever.
+                            // TODO: find a way to put the xor result into a virtual register
+                            --IIT;
 
-                    if (ConstantInt* CI = dyn_cast<ConstantInt>(Inst.getOperand(0))) {
-                        if (CI->getBitWidth() <= 32) {
-                            // Check if the value stored is zero
-                            if (CI->isZero()) {
 
-                                // We either target store instructions in the form : store i32 0, i32* %var
-                                // and return instructions.
-                                unsigned Opcode = Inst.getOpcode();
-                                // We insert instructions in the following form :
-                                //
-                                // %0 = load i32, i32* %var, align 4
-                                // %1 = xor i32 %0, %0
-                                // store i32 %1, i32* %var, align 4
-
-                                if (Opcode == Instruction::Store) {
-
-                                    Value *var_ptr = Inst.getOperand(1);
-                                    Value *var_load = Builder.CreateAlignedLoad(var_ptr, SI->getAlignment());
-                                    Value *NewValue = Builder.CreateAlignedStore(
-                                            Builder.CreateXor(var_load,
-                                                var_load), var_ptr, SI->getAlignment());
-                                    ReplaceInstWithValue(BB.getInstList(), IIT, NewValue);
-
-                                    // Consecutive zero assignments causes some instructions to be skipped.
-                                    // The instruction iterator is decremented to avoid this.
-                                    --IIT;
-                                }
-
-                                // inconsequent transformation
-                                if (Opcode == Instruction::Ret) {
-                                    Value *one_1 = ConstantInt::get(CI->getContext(), llvm::APInt(32, 1, true));
-                                    Value *one_2 = ConstantInt::get(CI->getContext(), llvm::APInt(32, 1, true));
-                                    Value *NewValue = Builder.CreateRet(
-                                            Builder.CreateXor(one_1, one_2));
-                                    ReplaceInstWithValue(BB.getInstList(), IIT, NewValue);
-
-                                    // Without this, the pass will just perform the transformation forever
-                                    // TODO: find a way to put the xor result into a virtual register
-                                    IIT--;
-                                }
-
-                                modified = true;
-                                XORCount = XORCount + 1;
-                            }
+                            modified = true;
+                            XORCount = XORCount + 1;
                         }
                     }
                 }
 
                 return modified;
+            }
+
+        private:
+
+            // We either target store instructions in the form : store i32 0, i32* %var
+            // and return instructions.
+            bool isValidCandidateInstruction(Instruction &Inst) {
+                if (isa<StoreInst>(&Inst)) {
+                    return true;
+                } else if (isa<ReturnInst>(&Inst)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            // Check if the value stored in a operand is zero
+            bool isValidCandidateOperand(Value *V) {
+                ConstantInt *CI;
+
+                if (ConstantInt* CI = dyn_cast<ConstantInt>(V)) {
+                    if (CI->getBitWidth() <= 32) {
+                        if (CI->isZero()) {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            Value *replaceZero(Instruction &Inst) {
+
+                IRBuilder<> Builder(&Inst);
+                unsigned Opcode = Inst.getOpcode();
+                // We insert instructions in the following form :
+                //
+                // %0 = load i32, i32* %var
+                // %1 = xor i32 %0, %0
+                // store i32 %1, i32* %var
+
+                if (Opcode == Instruction::Store) {
+
+                    Value *var_ptr = Inst.getOperand(1);
+                    Value *var_load = Builder.CreateLoad(var_ptr);
+                    Value *NewValue = Builder.CreateStore(
+                            Builder.CreateXor(var_load,
+                                var_load), var_ptr);
+                    return NewValue;
+                }
+
+                // inconsequent transformation
+                if (Opcode == Instruction::Ret) {
+                    Value *one_1 = ConstantInt::get(Inst.getOperand(0)->getType(), llvm::APInt(32, 1, true));
+                    Value *one_2 = ConstantInt::get(Inst.getOperand(0)->getType(), llvm::APInt(32, 1, true));
+                    Value *NewValue = Builder.CreateRet(
+                            Builder.CreateXor(one_1, one_2));
+                    return NewValue;
+                }
+
+                return nullptr;
             }
     };
 }
